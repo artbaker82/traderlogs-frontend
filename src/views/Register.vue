8cc76094d4817d038248f5ba432b52b1d89c080d
Arthur Baker <artbaker82@gmail.com>
2022-11-13 11:56:04 -0800

    starting registration page

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,5 +1,33 @@
 <template>
-  <p>sign up</p>
+    <div id="user-creds-container">
+
+      <div class="field">
+        <label class="label">Email</label>
+        <div class="control">
+          <input v-model="loginEmail" class="input" type="text" placeholder="email@email.com">
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Password</label>
+        <div class="control">
+          <input v-model="loginPassword" class="input" type="password" placeholder="Text input">
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Password Confirmation</label>
+        <div class="control">
+          <input v-model="loginPassword" class="input" type="password" placeholder="Text input">
+        </div>
+      </div>
+
+      <div class="control">
+        <button class="button is-link" @click="handleLogin">Login</button>
+        <router-link to="/Register">Or sign up here</router-link>
+      </div>
+
+  </div>
 </template>
 
 <script>
